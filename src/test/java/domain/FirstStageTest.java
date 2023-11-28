150ed6c279f3d8589a730064ef6964c0e44caef7
KDT live <dnjsxo0616@gmail.com>
2023-11-28 15:47:00 +0900

    test: findPlayerPosition 테스트 추가

--- src/test/java/domain/FirstStageTest.java
+++ src/test/java/domain/FirstStageTest.java
@@ -1,8 +1,10 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FirstStageTest {
@@ -33,4 +35,34 @@ class FirstStageTest {
 
         assertEquals(2, result);
     }
+
+    @Test
+    @DisplayName("플레이어_위치_검색_테스트")
+    void findPlayerPosition() {
+        int[][] testArray = {
+                {0, 4, 4, 0},
+                {0, 2, 2, 3},
+                {0, 4, 0, 4}
+        };
+
+        int[] result = FirstStage.findPlayerPosition(testArray);
+
+        assertArrayEquals(new int[]{2, 4}, result);
+    }
+
+    @Test
+    @DisplayName("플레이어_위치_오류_테스트")
+    void findPlayerNoposition() {
+        int[][] testArray = {
+                {0, 4, 4, 0},
+                {0, 2, 2, 0},
+                {0, 4, 0, 4}
+        };
+
+        assertThatThrownBy(() -> FirstStage.findPlayerPosition(testArray))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("플레이어가 존재하지 않습니다.");
+    }
+
+
 }
