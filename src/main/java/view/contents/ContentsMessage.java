7ea223ddf64e1e750b18e495dbcaba87c58b2d88
KDT live <dnjsxo0616@gmail.com>
2023-11-28 13:58:17 +0900

    feat: contentsMessage enum 추가

---
+++ src/main/java/view/contents/ContentsMessage.java
@@ -0,0 +1,23 @@
+package view.contents;
+
+public enum ContentsMessage {
+    STAGE_COUNT("Stage %d"),
+    WIDTH_SIZE("가로 길이: %d"),
+    HEIGHT_SIZE("세로 길이: %d"),
+    HOLE_COUNT("구멍의 수: %d"),
+    BALLS_COUNT("공의 수: %d"),
+    PLAYER_POSITION("플레이어 위치: %d행 %d열");
+
+    private final String message;
+    ContentsMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFormattedMessage(int count) {
+        return String.format(message, count);
+    }
+
+    public String getFormattedPosition(int row, int columns) {
+        return String.format(message, row, columns);
+    }
+}
