5ecc03a53b0f2c0a35eb7807c4d0fcc5b207d6d3
KDT live <dnjsxo0616@gmail.com>
2023-11-29 16:55:37 +0900

    refactor: MoveDirection enum 리팩토링
    - 각 방향에 대한 행 및 열 변경값을 추가.
    - 이동 방향뿐만 아니라 행 및 열의 변경 값을 얻을 수 있다.

--- src/main/java/domain/MoveDirection.java
+++ src/main/java/domain/MoveDirection.java
@@ -1,16 +1,20 @@
 package domain;
 
 public enum MoveDirection {
-    UP('w'),
-    LEFT('a'),
-    DOWN('s'),
-    RIGHT('d'),
-    QUIT('q');
+    UP('w', -1, 0),
+    LEFT('a', 0, -1),
+    DOWN('s', 1, 0),
+    RIGHT('d', 0, 1),
+    QUIT('q', 0, 0);
 
     private final char command;
+    private final int rowChange;
+    private final int colChange;
 
-    MoveDirection(char command) {
+    MoveDirection(char command, int rowChange, int colChange) {
         this.command = command;
+        this.rowChange = rowChange;
+        this.colChange = colChange;
     }
 
     public static MoveDirection fromCommand(char command) {
@@ -21,5 +25,13 @@ public enum MoveDirection {
             }
         }
         throw new IllegalArgumentException("(경고) 지원하지 않는 명령입니다!");
+    }
+
+    public int getRowChange() {
+        return rowChange;
+    }
+
+    public int getColChange() {
+        return colChange;
     }
 }
