695ec0668168fa7e2d9178bf367dc9c37259bfdc
KDT live <dnjsxo0616@gmail.com>
2023-11-29 18:44:02 +0900

    feat: 각 명령어 메시지 출력 기능 추가

--- src/main/java/domain/MoveDirection.java
+++ src/main/java/domain/MoveDirection.java
@@ -1,26 +1,29 @@
 package domain;
 
 public enum MoveDirection {
-    UP('W', -1, 0),
-    LEFT('A', 0, -1),
-    DOWN('S', 1, 0),
-    RIGHT('D', 0, 1),
-    QUIT('Q', 0, 0);
+    UP('W', -1, 0, "W: 위로 이동합니다."),
+    LEFT('A', 0, -1, "A: 왼쪽으로 이동합니다."),
+    DOWN('S', 1, 0, "S: 아래쪽으로 이동합니다."),
+    RIGHT('D', 0, 1, "S: 아래쪽으로 이동합니다."),
+    QUIT('Q', 0, 0, "Bye~");
 
     private final char command;
     private final int rowChange;
     private final int colChange;
+    private final String commandMessage;
 
-    MoveDirection(char command, int rowChange, int colChange) {
+    MoveDirection(char command, int rowChange, int colChange, String commandMessage) {
         this.command = command;
         this.rowChange = rowChange;
         this.colChange = colChange;
+        this.commandMessage = commandMessage;
     }
 
     public static MoveDirection fromCommand(char command) {
         char upperCommand = Character.toUpperCase(command);
         for (MoveDirection direction : values()) {
             if (direction.command == upperCommand) {
+                System.out.println(direction.commandMessage);
                 return direction;
             }
         }
